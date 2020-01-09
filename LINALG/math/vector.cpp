4fbd2a5a614caad9af42b9ab2e75af5ea08d7911
Tom Roozen <taamrooz@avans.nl>
2020-01-09 23:49:54 +0100

    update vectors

--- LINALG/math/vector.cpp
+++ LINALG/math/vector.cpp
@@ -1,5 +1,11 @@
 #include "vector.h"
+#include <cmath>
 #include "SDL.h"
+
+vector::vector()
+{
+	x = y = z = 0.0f;
+}
 
 vector::vector(float fx, float fy, float fz)
 {
@@ -38,6 +44,68 @@ vector& vector::operator*=(float v)
 	y *= v;
 	z *= v;
 	return *this;
+}
+
+vector& vector::operator/=(float v)
+{
+	x /= v;
+	y /= v;
+	z /= v;
+	return *this;
+}
+
+vector vector::operator+(const vector& v) const
+{
+	return vector{ x + v.x, y + v.y, z + v.z };
+}
+
+vector vector::operator-(const vector& v) const
+{
+	return vector{ x - v.x, y - v.y, z - v.z };
+}
+
+vector vector::operator*(const vector& v) const
+{
+	return vector{ x * v.x, y * v.y, z * v.z };
+}
+
+vector vector::operator*(float v) const
+{
+	return vector{ z * v, y * v, z * v };
+}
+
+vector vector::operator/(float v) const
+{
+	return vector{ x / v, y / v, z / v };
+}
+
+vector vector::cross(const vector& other) const
+{
+	vector vec;
+	vec.x = (y * other.z) - (z * other.y);
+	vec.y = (z * other.x) - (x * other.z);
+	vec.z = (x * other.y) - (y * other.x);
+	return vec;
+}
+
+float vector::length() const
+{
+	return std::sqrt(x * x + y * y + z * z);
+}
+
+void vector::normalize()
+{
+	auto length = this->length();
+	if (length != 0.0f)
+	{
+		x /= length;
+		y /= length;
+		z /= length;
+	}
+	else
+	{
+		x = y = 0.0f; z = 1.0f;
+	}
 }
 
 void vector::multiply(float scalar)
--- LINALG/math/vector.h
+++ LINALG/math/vector.h
@@ -4,11 +4,21 @@
 class vector
 {
 public:
+	vector();
 	vector(float, float, float);
 	vector& operator+=(const vector& v);
 	vector& operator-=(const vector& v);
 	vector& operator*=(const vector& v);
 	vector& operator*=(float v);
+	vector& operator/=(float v);
+	vector operator+(const vector& v) const;
+	vector operator-(const vector& v) const;
+	vector operator*(const vector& v) const;
+	vector operator*(float v) const;
+	vector operator/(float v) const;
+	vector cross(const vector& other) const;
+	float length() const;
+	void normalize();
 	void multiply(float scalar);
 	void render(SDL_Renderer* renderer_, int k_screen_width, int k_screen_height);
 	float x, y, z;
